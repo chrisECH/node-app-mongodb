b0971701fbccdfbd697fa66db1bea0198dec3e83
Christian Eduardo <chris.e1297@gmail.com>
2023-02-08 20:03:25 -0600

    Configuracion inicial servidor (Express)

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.get('/',(req, res)=>{
 });
 
 // Static files
-qpp.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 module.exports = app
