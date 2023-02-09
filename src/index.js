891fc2d2544b89bfe2363549f742588f24893cc1
Christian Eduardo <chris.e1297@gmail.com>
2023-02-08 20:25:04 -0600

    Configuracion Base de datos (MongoDB)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const app = require('./server')
+require('dotenv').config();
+
+const app = require('./server');
+require('./database');
+
 
 app.listen(app.get('port'), () => {
     console.log('listening on port', app.get('port'));
